bcce3549350aa1d5f208101e00f00f321972e0ef
mtiapko <fithisback@gmail.com>
2021-04-29 23:09:09 +0300

    reworked 'grouped_enumerators' to 'enumerators_grouped_by_value'

--- src/AST/Visitor.cpp
+++ src/AST/Visitor.cpp
@@ -1,3 +1,5 @@
+#include <charconv>
+
 #include <clang/Basic/SourceManager.h>
 #include <clang/AST/RecordLayout.h>
 #include <clang/Lex/Lexer.h>
@@ -265,46 +267,37 @@ void Visitor::gen_enum_decl_content(const clang::EnumDecl* decl,
 
 	content["integer_type"] = decl->getIntegerType().getAsString(m_printing_policy);
 
-	struct EnumeratorValue
-	{
-		const bool is_signed;
-		const int64_t data;
-
-		EnumeratorValue(const llvm::APSInt& value) noexcept
-			: is_signed(value.isSigned())
-			, data(value.getExtValue())
-		{}
-	};
-
-	std::map<EnumeratorValue, inja::json, decltype(
-		[](const auto& lhs, const auto& rhs)
-		{
-			assert(lhs.is_signed == rhs.is_signed);
-			return (lhs.is_signed
-				? (lhs.data < rhs.data)
-				: (static_cast<uint64_t>(lhs.data) < static_cast<uint64_t>(rhs.data)));
-		}
-	)> grouped_enumerators_map;
-
-	auto& enumerators_content = content["enumerators"];
+	auto& enumerators_content                  = content["enumerators"];
+	auto& enumerators_grouped_by_value_content = content["enumerators_grouped_by_value"];
 	for (const auto& decl_enumerator: decl->enumerators()) {
 		auto& e = enumerators_content.emplace_back();
 
 		const auto& enumerator_name = decl_enumerator->getName();
 		e["name"] = enumerator_name;
 
-		auto& e_value = e["value"];
+		auto& e_value_content = e["value"];
 
-		EnumeratorValue value { decl_enumerator->getInitVal() };
-		if (value.is_signed) e_value = value.data;
-		else e_value = static_cast<uint64_t>(value.data);
+		// NOTE(FiTH): 'end() - 1' - leave place for '\0'
+		std::array<char, 32> enum_init_val_str {};
 
-		grouped_enumerators_map[value].emplace_back(enumerator_name);
+		const auto& enum_init_val = decl_enumerator->getInitVal();
+		if (enum_init_val.isSigned()) {
+			int64_t signed_value = enum_init_val.getExtValue();
+			e_value_content = signed_value;
+
+			auto [ptr, ec] = std::to_chars(enum_init_val_str.begin(), enum_init_val_str.end() - 1, signed_value);
+			*ptr = '\0';
+		} else {
+			uint64_t unsigned_value = enum_init_val.getZExtValue();
+			e_value_content = unsigned_value;
+
+			auto [ptr, ec] = std::to_chars(enum_init_val_str.begin(), enum_init_val_str.end() - 1, unsigned_value);
+			*ptr = '\0';
+		}
+
+		// key: enumerators value -> data: array of enumerators (all have the same value)
+		enumerators_grouped_by_value_content[enum_init_val_str.data()] += e;
 	}
-
-	auto& grouped_enumerators = content["grouped_enumerators"];
-	for (const auto& [enumerators_value, enumerators]: grouped_enumerators_map)
-		grouped_enumerators.emplace_back(std::move(enumerators));
 }
 
 void Visitor::gen_func_decl_content(const clang::FunctionDecl* decl,

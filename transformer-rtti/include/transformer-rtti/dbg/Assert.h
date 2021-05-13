ebca6598ab3365242a6a3fb146fa93d204322087
mtiapko <fithisback@gmail.com>
2021-05-13 10:10:30 +0300

    added missing Config.h file and transformer-rtti/dbg dir

---
+++ transformer-rtti/include/transformer-rtti/dbg/Assert.h
@@ -0,0 +1,10 @@
+#ifndef __TRANSFORMER_RTTI_DBG_ASSERT_H__
+#define __TRANSFORMER_RTTI_DBG_ASSERT_H__
+
+#include <assert.h> // TODO(FiTH): remove
+
+#include "transformer-rtti/Config.h"
+
+#define RTTI_ASSERT(x, ...) assert(x)
+
+#endif // !__TRANSFORMER_RTTI_DBG_ASSERT_H__

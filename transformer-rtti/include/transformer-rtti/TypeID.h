d35638108eda5e4db233cb9ac86ab556679b27aa
mtiapko <fithisback@gmail.com>
2021-05-27 21:53:45 +0300

    rtti: added TypeID.h

---
+++ transformer-rtti/include/transformer-rtti/TypeID.h
@@ -0,0 +1,13 @@
+#ifndef __TRANSFORMER_RTTI_TYPE_ID_H__
+#define __TRANSFORMER_RTTI_TYPE_ID_H__
+
+#include <stdint.h>
+
+namespace transformer::rtti
+{
+
+using TypeID = uint32_t;
+
+}
+
+#endif // !__TRANSFORMER_RTTI_TYPE_ID_H__
